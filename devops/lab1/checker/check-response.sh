07b0d1ac7fd41e3ed5d1add6ca139745f452d4b6
Denys Starchenko <dvs8883@gmail.com>
2023-04-21 00:26:30 +0300

    fix script

--- devops/lab1/checker/check-response.sh
+++ devops/lab1/checker/check-response.sh
@@ -4,7 +4,7 @@ status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://l
 
 if [[ "$status_code" -lt 200 || "$status_code" -gt 399 ]]; then
   message="Wrong response from the server api. Http code - $status_code"
-  echo "$message" > /app/log/test-api-response.log
+  echo "$message" >> /app/log/test-api-response.log
   echo "$message" | mail -s "HTTP STATUS CHECKER" "Denys.Starchenko@infiz.khpi.edu.ua"
 else
   echo "$status_code"
